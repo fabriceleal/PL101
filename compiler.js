f7a58ef1ee0951dc4d06fe45cdd632bab9f5538d
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-04-15 12:19:28 -0700

    added notes dictionary, to convert from pitch to midi nbr

--- compiler.js
+++ compiler.js
@@ -27,7 +27,7 @@ var reverse = function(expr) {
 var endTime = function (time, expr) {
     var totalDuration = function(expr){
         // The duration of a note node is the duration itself
-        if(expr.tag=='note')
+        if(expr.tag=='note' || expr.tag == 'rest')
             return expr.dur;
         
         // The duration of a par node is the maximum of 
@@ -46,6 +46,61 @@ var endTime = function (time, expr) {
     return time + totalDuration(expr);
 };
 
+var notes = {
+	'a0' : 21,
+	'b0' : 23,
+	'c1' : 24,
+	'd1' : 26,
+	'e1' : 28,
+	'f1' : 29,
+	'g1' : 31,
+	'a1' : 33,
+	'b1' : 35,
+	'c2' : 36,
+	'd2' : 38,
+	'e2' : 40,
+	'f2' : 41,
+	'g2' : 43,
+	'a2' : 45,
+	'b2' : 47,
+	'c3' : 48,
+	'd3' : 50,
+	'e3' : 52,
+	'f3' : 53,
+	'g3' : 55,
+	'a3' : 57,
+	'b3' : 59,
+	'c4' : 60,
+	'd4' : 62,
+	'e4' : 64,
+	'f4' : 65,
+	'g4' : 67,
+	'a4' : 69,
+	'b4' : 71,
+	'c5' : 72,
+	'd5' : 74,
+	'e5' : 76,
+	'f5' : 77,
+	'g5' : 79,
+	'a5' : 81,
+	'b5' : 83,
+	'c6' : 84,
+	'd6' : 86,
+	'e6' : 88,
+	'f6' : 89,
+	'g6' : 91,
+	'a6' : 93,
+	'b6' : 95,
+	'c7' : 96,
+	'd7' : 98,
+	'e7' : 100,
+	'f7' : 101,
+	'g7' : 103,
+	'a7' : 105,
+	'b7' : 107,
+	'c8' : 108
+};
+
 
 var compileT = function(expr, time){
     var ret = [];
@@ -53,10 +108,17 @@ var compileT = function(expr, time){
     if(expr.tag == 'note'){
         ret.push({
             tag : 'note',
-            pitch: expr.pitch,
+            pitch: notes[expr.pitch],
             start: time,
             dur: expr.dur
         });
+    }
+    if(expr.tag == 'rest'){
+	ret.push({
+	   tag : 'rest',
+	   start: time,
+	   dur : expr.dur
+	});
     }
     if(expr.tag == 'par'){
         // Call on left and right with the same starting time
@@ -112,6 +174,7 @@ var melody_mus =
 
 console.log(melody_mus);
 console.log(compile(melody_mus));
+
 
 
 
